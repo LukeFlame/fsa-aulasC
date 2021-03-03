569d04766be760cc9ed3928516b69c5ff76a6732
LukeFl_ <69337083+LukeFlame@users.noreply.github.com>
2021-03-02 22:29:29 -0300

    comments added

--- aula02.c
+++ aula02.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
 int main(int argc, char *argv[]) {
+	// Declara os valores a receber
 	int n1=0, n2=0;
+	
+	// Recebe os valores
 	printf("Digite o primeiro valor:\n");
 	scanf("%i",&n1);
 	
 	printf("Digite o segundo valor:\n");
 	scanf("%i",&n2);
 	
+	// Processa os resultados
 	int resultAdd = n1 + n2,
 		resultSub = n1 - n2,
 		resultMult = n1 * n2;
 	double resultDiv = n1 / n2;
 	
+	// Mostra os resultados
 	printf("Adicao: %i\n", resultAdd);
 	printf("Subtracao: %i\n", resultSub);
 	printf("Multiplicacao: %i\n", resultMult);
