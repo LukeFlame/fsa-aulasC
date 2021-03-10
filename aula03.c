284cb997306885ed65bac4c0804b070436e760ce
LukeFl_ <69337083+LukeFlame@users.noreply.github.com>
2021-03-09 22:44:18 -0300

    comments added

--- aula03.c
+++ aula03.c
@@ -4,11 +4,14 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+	// Inicializa variáveis
 	int n, y, result = 0;
 	
-	printf("Digite qua tabuada deseja:\n");
+	// Pergunta a tabuada
+	printf("Digite qual tabuada deseja:\n");
 	scanf("%i",&y);
 	
+	// Cria e mostra a tabuada
 	for (n = 0; n <= 20; n++) {
 		result = n * y;
 		printf("%i x %i = %i\n", y, n, result);
